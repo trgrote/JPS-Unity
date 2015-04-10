a331152900c277936339c50e399aa3517b870074
Taylor Grote <grotese@gmail.com>
2015-04-09 23:13:19 -0500

    Initial Commit

---
+++ Assets/Scripts/BlockScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum eDirections
+{
+	DIR_NORTH,
+	DIR_NORTH_EAST,
+	DIR_EAST,
+	DIR_SOUTH_EAST,
+	DIR_SOUTH,
+	DIR_SOUTH_WEST,
+	DIR_WEST,
+	DIR_NORTH_WEST,
+}
+
+[RequireComponent(typeof(Collider2D))]
+public class BlockScript : MonoBehaviour
+{
+	public bool isJumpPoint = false;
+	public bool isObstacle = false;
+
+	public Sprite passableSprite = null;
+	public Sprite obstacleSprite = null;
+
+	public Dictionary< eDirections, int > wallDistances = new Dictionary<eDirections, int>(8);
+
+	void setSprite()
+	{
+		GetComponent<SpriteRenderer>().sprite = isObstacle == false ? 
+			GetComponent<SpriteRenderer>().sprite = passableSprite :
+			GetComponent<SpriteRenderer>().sprite = obstacleSprite;
+	}
+
+	void OnMouseDown()
+	{
+		isObstacle = !isObstacle;
+		setSprite();
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		setSprite();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+}
---
+++ Assets/Scripts/GridView.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class GridView : MonoBehaviour 
+{
+	public GameObject blockPrefab = null;
+
+	[RangeAttribute(0, 255)]
+	public int numBlocks = 1;
+	private int previousNumBlocks = 0;
+	public float blockSize = 0.64f;
+	public int rowSize = 10;
+
+	private GameObject[] childObjects = new GameObject[1];
+
+	void resize()
+	{
+		// Kill all my children
+		foreach ( GameObject child in childObjects )
+		{
+			DestroyImmediate( child );
+		}
+
+		childObjects = new GameObject[numBlocks];
+
+		for ( int i = 0; i < numBlocks ; ++i )
+		{
+			// Create a new Child object
+			GameObject child = Instantiate( blockPrefab );
+			child.GetComponent<Transform>().parent = GetComponent<Transform>();  // Set as parent of this new child
+			child.GetComponent<Transform>().localPosition = new Vector3(
+				( i % rowSize ) *  blockSize,
+				( i / rowSize ) * -blockSize,
+				0.0f
+			);
+
+			childObjects[ i ] = child;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		// If no one has given us a prefab to use, then don't make anything as we'll just get null pointer exception nonsense
+		if ( blockPrefab == null )
+			return;
+
+		// If we need to resize then do
+		if ( previousNumBlocks != numBlocks )
+		{
+			resize();
+			previousNumBlocks = numBlocks;
+		}
+	}
+}
