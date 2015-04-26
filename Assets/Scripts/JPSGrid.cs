e59b78906cb3db341537006a1f154d41d95c43fd
Taylor Grote <grotese@gmail.com>
2015-04-25 19:52:07 -0500

    Added JPSGrid File to sep JPS logic and Unity.

---
+++ Assets/Scripts/JPSGrid.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum eDirections
+{
+	DIR_NORTH      = 0,
+	DIR_NORTH_EAST = 1,
+	DIR_EAST       = 2,
+	DIR_SOUTH_EAST = 3,
+	DIR_SOUTH      = 4,
+	DIR_SOUTH_WEST = 5,
+	DIR_WEST       = 6,
+	DIR_NORTH_WEST = 7,
+}
+
+public class Node
+{
+	public int[] jpDistances = new int[8];
+	public bool isObstacle     = false;
+	public bool isJumpPoint    = false;
+}
+
+public class Grid
+{
+	public Node[] gridNodes = new Node[0];
+}
